3b904d86286ca333cdd2734273aadc5ffb549ade
afzal-10-02 <afzalj_jamal@srmus.edu.in>
2024-09-14 20:29:07 +0530

    Merge Sort Added

---
+++ Sorting/MergeSort.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+
+#Example 
+array = [4,8,9,1,2,-7,-5,12,10]
+sorted_array = merge_sort(array)
+
+#Print the Sorted Array
+print(f'Sorted Array is : {sorted_array}')
